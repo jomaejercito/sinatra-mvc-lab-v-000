056a8334ee9bbf4af833e4f098082e4f4adb4703
Joma Ejercito <jomaejercito@gmail.com>
2018-04-12 20:40:44 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -12,7 +12,7 @@ class PigLatinizer
       first_split = word.split(/([aeiouAEIOU].*)/)[0]
       second_split = word.split(/([aeiouAEIOU].*)/)[1]
       if first_split == ""
-        second_split + "way"
+         second_split + "way"
       else
         second_split + first_split + "ay"
       end
