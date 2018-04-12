0b50dc1ddf86b6ff72b5e3e07d21d7f20334ec6f
Joma Ejercito <jomaejercito@gmail.com>
2018-04-12 20:45:55 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,5 +1,10 @@
 class PigLatinizer
 
+#For words that begin with consonant sounds, all letters before the initial vowel are placed at the end of the word sequence. Then, "ay" is added.
+#When words begin with consonant clusters (multiple consonants that form one sound), the whole sound is added to the end when speaking or writing.
+#For words that begin with vowel sounds, one just adds "way" or "yay" to the end (or just "ay").
+
+
   def to_pig_latin(sentence)
     word = sentence.split(" ")
     word.map! {|word| piglatinize(word)}.join(" ")
